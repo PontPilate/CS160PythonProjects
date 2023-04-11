b455918d0a2d815666356fa875a4a1be087ec259
PontPilate <130502536+PontPilate@users.noreply.github.com>
2023-04-11 12:20:39 -0700

    Add files via upload

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-nothing
+import turtle
+t = turtle.Turtle()
+from time import sleep
+
+#square
+#t.pen does not work in sandbox for whatever god foresaken reason.
+##t.pen(fillcolor="green", pencolor="green", pensize=20)
+t.shape('square')
+
+
+#roof
+
+
+#sun
+t.pu()
+t.setpos(150, 190)
+t.shape('circle')
